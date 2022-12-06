f68de4054c4d7f50316b29014e1b9e45d5fe70c6
Dymmon <carlos.montuyaog@utem.cl>
2022-12-06 14:41:44 -0300

    added guard unit testing, frontend statement to 95.1%

--- frontend/src/app/guards/init.guard.spec.ts
+++ frontend/src/app/guards/init.guard.spec.ts
@@ -1,16 +1,29 @@
 import { TestBed } from '@angular/core/testing';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { rutReducer } from '../components/redux/reducers/rut.reducer';
 
 import { InitGuard } from './init.guard';
 
 describe('InitGuard', () => {
   let guard: InitGuard;
-
+  let routerSpy = {navigate: jasmine.createSpy('navigate')};
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[
+        StoreModule.forRoot({rut: rutReducer})],
+      providers: [
+        { provide: Router, useValue: routerSpy }
+      ]
+    });
     guard = TestBed.inject(InitGuard);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
   });
+  it('should do something', () => {
+    const result = guard.canActivate(new ActivatedRouteSnapshot(), <RouterStateSnapshot>{url: 'testUrl'});
+    expect(result).toBeFalsy();
+  })
 });
